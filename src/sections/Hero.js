2ab6a7c839c627325764a3d0c163a3fd4192b129
artemis prime <artemisprimedev@gmail.com>
2020-04-07 09:00:31 -0700

    jay finished

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default (props) => (
   <div className='hero'>
+    <header>
 
-    
+    </header>
+    <main>
+      <img className='logo' src='assets/trillerfest-logo-735x276.png' alt='tf' />
+
+      <div className='content-outer'>
+        <img className='subscribe' src='assets/ty-subscribe-free-pass.png' alt='tf' />
+        <div className='copy-outer'>
+        <p className='line-1'>
+            3 days of quarantine sessions
+        </p>
+        <p className='line-2'>
+          Fri 4/10 - Sun 4/12
+        </p>
+        <p className='line-3'>
+          the largest virtual live music concert ever.
+        </p>
+        </div>
+      </div>
+
+    </main>
   </div>
-
 )
