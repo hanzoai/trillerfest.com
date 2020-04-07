392894efa4bfcddfc564dc8986579eac6f1136d5
Zach Kelling <z@zeekay.io>
2020-04-07 11:49:57 -0700

    Update social links

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -8,10 +8,10 @@ export default (props) => {
 
   return (
     <div className='social-icons-outer'>
-      <SocialIcon network="facebook" bgColor='transparent' fgColor='white'/>
-      <SocialIcon network="twitter"  bgColor='transparent' fgColor='white'/>
-      <SocialIcon network="youtube"  bgColor='transparent' fgColor='white'/>
-      <SocialIcon network="instagram"  bgColor='transparent' fgColor='white'/>
+      <SocialIcon network="facebook"  url="https://facebook.com/trillervids"  bgColor='transparent' fgColor='white'/>
+      <SocialIcon network="twitter"   url="https://twitter.com/triller"       bgColor='transparent' fgColor='white'/>
+      <SocialIcon network="youtube"   url="https://youtube.com/trillervids"   bgColor='transparent' fgColor='white'/>
+      <SocialIcon network="instagram" url="https://instagram.com/trillervids" bgColor='transparent' fgColor='white'/>
     </div>
   )
 }
