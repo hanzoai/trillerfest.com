607dce58da50afc955c2235256cba8c44f07f2ae
Zach Kelling <z@zeekay.io>
2020-04-08 14:59:45 -0700

    Hide social icons for now

--- src/sections/JayPharoah.js
+++ src/sections/JayPharoah.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import SocialIcons from '../components/SocialIcons'
+// import SocialIcons from '../components/SocialIcons'
+// <SocialIcons size='50px' />
 
 export default (props) => (
   <div className='jay-pharoah' >
@@ -9,6 +10,5 @@ export default (props) => (
       <h2>Jay Pharoah</h2>
     </div>
     <img src='assets/jay-589x677.png' alt='jay' />
-    <SocialIcons size='50px' />
   </div>
 )
