6a2a4a5f69756c0c9543d0bcc8a827554dcae813
artemis prime <artemisprimedev@gmail.com>
2020-04-07 21:41:10 -0700

    instafeed done

--- src/sections/InstaFeed.js
+++ src/sections/InstaFeed.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import YouTubeSubscribe from '../components/YouTubeSubscribe'
 
+import YouTubeSubscribe from '../components/YouTubeSubscribe'
+import InstagramMatrix from '../components/InstagramMatrix'
 
 
 export default (props) => (
   <div className='instafeed' >
     <h2>Triller Insta Feed</h2>
+    <InstagramMatrix size={150} />
+    <YouTubeSubscribe />
+  </div>
+)
+
+/* 
     <a href="https://instagram.com/trillervids">
       <img src='assets/insta-whole-1109x1068.png' width='1109px' height='1068px' alt='insta' className='insta'/>
     </a>
-    <YouTubeSubscribe />
-  </div>
-
-)
+*/
