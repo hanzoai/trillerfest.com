df35944d73bebd412a8ec367322bdb0157c6eea9
David Tai <david@verus.io>
2020-04-09 13:12:15 -0500

    slidehsow background

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,9 +1,74 @@
 import React from 'react'
 import YouTubeSubscribe from '../components/YouTubeSubscribe'
+import { Zoom } from 'react-slideshow-image'
+
+const slideImages = [
+  {
+    img: 'assets/artists/SS_AJR.jpg',
+  },
+  {
+    img: 'assets/artists/SS_BLVK-JVCK.jpg',
+  },
+  {
+    img: 'assets/artists/SS_HANNAH.jpg',
+  },
+  {
+    img: 'assets/artists/SS_IDA.jpg',
+  },
+  {
+    img: 'assets/artists/SS_JEREMY-TORRES.jpg',
+  },
+  {
+    img: 'assets/artists/SS_MIGOS.jpg',
+  },
+  {
+    img: 'assets/artists/SS_Omicron.jpg',
+  },
+  {
+    img: 'assets/artists/SS_Paris.jpg',
+  },
+  {
+    img: 'assets/artists/SS_Sulan.jpg',
+  },
+  {
+    img: 'assets/artists/SS_YK-TOON.jpg',
+  },
+  {
+    img: 'assets/artists/SS_wyclef.jpg',
+  },
+];
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffleArray(slideImages)
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: false,
+  arrows: false,
+  pauseOnHover: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  }
+}
 
 export default (props) => (
   <div className='hero'>
     <main>
+      <Zoom className='hero-slider' {...properties}>
+        {
+          slideImages.map((img) => (
+            <div className='hero-slide' style={{'backgroundImage': `url(${img.img})`}}/>
+          ))
+        }
+      </Zoom>
       <img className='logo' src='assets/trillerfest-logo-735x276.png' alt='tf'/>
       <div className='content-outer'>
         <YouTubeSubscribe />
