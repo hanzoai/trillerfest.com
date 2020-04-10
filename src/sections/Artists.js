9370832081757940607d7b750941d125f269eb86
David Tai <david@verus.io>
2020-04-10 12:02:53 -0500

    add lineup

--- src/sections/Artists.js
+++ src/sections/Artists.js
@@ -4,6 +4,7 @@ import YouTubeSubscribe from '../components/YouTubeSubscribe'
 export default (props) => (
   <div className='artists'>
     <main>
+      <img className='lineup' src='assets/FB_FRIDAYlineup_web.png' alt='lineup'/>
       <img className='artist-names' src='assets/artists.png' alt='artists'/>
       <div className='content-outer'>
         <YouTubeSubscribe />
