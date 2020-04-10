6a38628edfab8145e81dab0e11d442bddd85d245
David Tai <david@verus.io>
2020-04-10 12:34:53 -0500

    watch now buttons everywhere

--- src/sections/Artists.js
+++ src/sections/Artists.js
@@ -7,9 +7,16 @@ export default (props) => (
       <img className='lineup' src='assets/FB_FRIDAYlineup_web.png' alt='lineup'/>
       <img className='artist-names' src='assets/artists.png' alt='artists'/>
       <div className='content-outer'>
-        <YouTubeSubscribe />
+        <a href='https://www.youtube.com/c/trillervids?sub_confirmation=1' target='_blank'>
+          <img className='watch-button' src='assets/yt-watch.png' alt='Watch on Youtube'/>
+        </a>
+        <a href='https://www.twitch.tv/triller' target='_blank'>
+          <img className='watch-button' src='assets/twitch-watch.png' alt='Watch on Twitch'/>
+        </a>
+        <a href='https://www.caffeine.tv/Triller' target='_blank'>
+          <img className='watch-button' src='assets/caffeine-watch.png' alt='Watch on Caffeine'/>
+        </a>
       </div>
-
     </main>
   </div>
 )
--- src/sections/InstaFeed.js
+++ src/sections/InstaFeed.js
@@ -11,11 +11,22 @@ export default (props) => {
   <div className='instafeed' >
     <h2>Triller Insta Feed</h2>
     <InstagramMatrix size={(width > 451) ? 150 : 320} />
-    <YouTubeSubscribe />
+
+    <div className='content-outer'>
+      <a href='https://www.youtube.com/c/trillervids?sub_confirmation=1' target='_blank'>
+        <img className='watch-button' src='assets/yt-watch.png' alt='Watch on Youtube'/>
+      </a>
+      <a href='https://www.twitch.tv/triller' target='_blank'>
+        <img className='watch-button' src='assets/twitch-watch.png' alt='Watch on Twitch'/>
+      </a>
+      <a href='https://www.caffeine.tv/Triller' target='_blank'>
+        <img className='watch-button' src='assets/caffeine-watch.png' alt='Watch on Caffeine'/>
+      </a>
+    </div>
   </div>
 )}
 
-/* 
+/*
     <a href="https://instagram.com/trillervids">
       <img src='assets/insta-whole-1109x1068.png' width='1109px' height='1068px' alt='insta' className='insta'/>
     </a>
